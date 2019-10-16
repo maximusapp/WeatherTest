a9688989fb73f50ce1714fcd89139b9ad6bdc5a8
maxim_syrovatka <sportmaxful@gmail.com>
2019-10-16 11:39:08 +0300

    Add rx and rxBinding dependencies to project

--- app/src/main/java/com/weather/weathermain/domain/interactor/WeatherInteractor.kt
+++ app/src/main/java/com/weather/weathermain/domain/interactor/WeatherInteractor.kt
@@ -6,6 +6,9 @@ interface IWeatherInteractor {
     companion object {
         fun getInstance(weather: IWeatherRepository = IWeatherRepository.getInstance()) = WeatherInteractor(weather)
     }
+
+    fun getCurrentWeatherData(): Sing
+
 }
 
 class WeatherInteractor (private val weatherRepository: IWeatherRepository): IWeatherInteractor { }
