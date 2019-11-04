e6bd676dde60b7ad79250cd78ba73dc36731f580
maxim_syrovatka <sportmaxful@gmail.com>
2019-11-04 14:35:53 +0200

    small refactor

--- app/src/main/java/com/weather/weathermain/data/repository/WeatherRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.weather.weathermain.data.repository
-
-import com.weather.weathermain.data.WeatherOnTodayResponse
-import io.reactivex.Single
-
-interface WeatherRepository {
-    fun getCurrentWeatherData(latitude: Double, longitude: Double, units: String, appid: String): Single<WeatherOnTodayResponse>
-}
